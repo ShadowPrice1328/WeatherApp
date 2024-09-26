45c399fb79329d9469b9659e5eb69062b834f9c0
Anna Sonchak <60846759+ShadowPrice1328@users.noreply.github.com>
2024-09-26 19:47:28 +0200

    Loaded Constants.cs

---
+++ ServiceContracts/Data/Constants.cs
@@ -0,0 +1,9 @@
+namespace ServiceContracts.Data
+{
+    public static class Constants
+    {
+        public const string ApiKeyHeaderName = "Weather-API-Key";
+
+        public const string ApiKeyName = "ApiKey";
+    }
+}
