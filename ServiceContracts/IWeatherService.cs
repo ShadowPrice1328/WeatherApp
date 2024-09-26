a7cff85b21214979f193c75e75b98dcecf62950b
Anna Sonchak <60846759+ShadowPrice1328@users.noreply.github.com>
2024-09-26 19:47:51 +0200

    Add files via upload

---
+++ ServiceContracts/IWeatherService.cs
@@ -0,0 +1,10 @@
+﻿using Models;
+
+namespace ServiceContracts
+{
+    public interface IWeatherService
+    {
+        Task<string?> GetJsonResponseAsync(string city);
+        Task<WeatherResponse> GetWeatherResponseAsync(string city);
+    }
+}
